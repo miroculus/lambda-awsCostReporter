821be061d384515e0b34cb37c3fd7979eacaf84c
Matias De Carli <matiasdecarli@gmail.com>
2017-05-09 15:46:41 -0400

    added monthly to the deployment

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,27 @@ zip -r deploy.zip . -x *.git*
 
 # Upload to AWS Lambda & Run
 aws lambda update-function-code --function-name myDailyReport --zip-file fileb://deploy.zip --region us-west-2
-aws lambda invoke --function-name myDailyReport output.txt --region us-west-2
+
+# To excec after a deploy
+## aws lambda invoke --function-name myDailyReport output.txt --region us-west-2
+
+# Remove ZIP file
+rm deploy.zip output.txt
+
+########## Monthly Report
+
+# Rename $REPORT_TYPE.package.json to package.json since its the file that
+# Lambda uses to know which file in the ZIP is going to excec
+cp monthly_report.package.json package.json
+
+# Create ZIP file
+zip -r deploy.zip . -x *.git*
+
+# Upload to AWS Lambda & Run
+aws lambda update-function-code --function-name myMonthlyReport --zip-file fileb://deploy.zip --region us-west-2
+
+# To excec after a deploy
+## aws lambda invoke --function-name myMonthlyReport output.txt --region us-west-2
 
 # Remove ZIP file
 rm deploy.zip output.txt
